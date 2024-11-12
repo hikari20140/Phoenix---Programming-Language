298f50060e206faafcd65010f868d38efdfa750e
hikari20140 <160013755+hikari20140@users.noreply.github.com>
2024-11-12 19:03:38 +0900

    Update Phoenix.py

--- Phoenix.py
+++ Phoenix.py
@@ -1,6 +1,10 @@
 from curses.ascii import isdigit
 import re
 
+var = []
+var_i = []
+var_t = []
+
 def error(strs):
     print("error!" + strs)
 
@@ -33,6 +37,9 @@ def get_types(strs):
                     error(" " + b['typess'] + "型が期待されましたが、" + get_types(bs)['typess'] + "型が出力されました。")
             types = "array<" + str(b['typess']) + ">"
             value = strs
+    elif strs in var:
+        types = var_t[var.index(strs)]
+        value = var_i[var.index(strs)]
     return dict(typess=types,values=value)
 
 def read(path):
@@ -50,11 +57,25 @@ def check_call(token):
 def run(path):
     code = read(path)
     for b in code:
-        token = re.split('([();])', b)
+        token = re.split('([();])| ', b)
+        m = 0
+        for n in token:
+            if not n == None:
+                token[m] = n.replace('\n', '')
+                m += 1
         if token[0] == "print":
             if check_call(token):
-                print(get_types(token[2])['typess'])
+                print(get_types(token[2])['values'])
             else:
                 error(" 呼び出されると期待していましたが、呼び出されませんでした expect '()'")
+        if token[0] == "var" or token[0] == "let":
+            var.append(token[1])
+            if token[2] == "=":
+                var_i.append(get_types(token[3])['values'])
+                var_t.append(get_types(token[3])['typess'])
+            else:
+                var_i.append("null")
+                var_t.append("any")
+
 
 run('./test.phe')
